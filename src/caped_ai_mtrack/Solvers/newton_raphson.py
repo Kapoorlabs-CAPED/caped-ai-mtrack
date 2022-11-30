fd9d1738be4be86a2ed8a510fb366137cbd6afff
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-11-30 23:39:52 +0100

    newton raph

--- src/caped_ai_mtrack/Solvers/newton_raphson.py
+++ src/caped_ai_mtrack/Solvers/newton_raphson.py
@@ -16,15 +16,59 @@ class NewtonRaphson:
         self.polyfuncdiff = 0
         self.delpolyfuncdiff = 0
         self.secdelpolyfuncdiff = 0
+        self.MAX_ITER = 1000000
+        self.MIN_CHANGE = 1.0e-3
 
-    def updatePowCache(self, xc: float):
+    def _updatePowCache(self, xc: float):
 
         for j in range(-3, self.degree):
-            self.powcache[j + 3] = math.pow(self.xc, j)
+            self.powcache[j + 3] = math.pow(xc, j)
 
-    # def run(self):
+    def run(self, x, y):
 
-    def distance(self, minx, miny, maxx, maxy):
+        self._updatePowCache(self.xc)
+        self._computeFunctions()
+        iteration = 0
+
+        while abs(self.xcnew - self.xc) > self.MIN_CHANGE:
+
+            self.xc = self.xcnew
+            dmin = (self.polyfunc - y) * self.polyfuncdiff + (self.xc - x)
+            dmindiff = (
+                self.polyfuncdiff * self.polyfuncdiff
+                + (self.polyfunc - y) * self.delpolyfuncdiff
+                + 1
+            )
+            dminsecdiff = (
+                (self.polyfunc - y) * self.secdelpolyfuncdiff
+                + self.delpolyfuncdiff * self.polyfuncdiff
+                + 2 * self.polyfuncdiff * self.delpolyfuncdiff
+            )
+
+            iteration = iteration + 1
+            self._iterate(self.xc, dmin, dmindiff, dminsecdiff)
+            if math.isnan(self.xcnew):
+                self.xcnew = self.xc
+            self.delpolyfuncdiff = 0
+            self.polyfunc = 0
+            self.polyfuncdiff = 0
+            self.secdelpolyfuncdiff = 0
+
+            self._updatePowCache(self.xcnew)
+            self._computeFunctions()
+
+            if iteration >= self.MAX_ITER:
+                break
+
+        self.polyfunc = 0
+        for j in range(self.degree):
+            self.polyfunc = self.polyfunc + self.coeff[j] * math.pow(
+                self.xc, j
+            )
+
+        return self._distance(x, y, self.xc, self.polyfunc)
+
+    def _distance(self, minx, miny, maxx, maxy):
 
         distance = (maxx - minx) * (maxx - minx) + (maxy - miny) * (
             maxy - miny
@@ -32,7 +76,7 @@ class NewtonRaphson:
 
         return np.sqrt(distance)
 
-    def computeFunctions(self):
+    def _computeFunctions(self):
 
         for j in range(0, self.degree):
 
@@ -47,7 +91,12 @@ class NewtonRaphson:
                 self.delpolyfuncdiff + c * self.powcache[j + 1]
             )
 
-    def iterate(self, function, functionderiv, functionsecondderiv):
+            c = c * (j + self.degree - 2)
+            self.secdelpolyfuncdiff = (
+                self.secdelpolyfuncdiff + c * self.powcache[j]
+            )
+
+    def _iterate(self, function, functionderiv, functionsecondderiv):
 
         self.xcnew = self.xc - (function / functionderiv) * (
             1
