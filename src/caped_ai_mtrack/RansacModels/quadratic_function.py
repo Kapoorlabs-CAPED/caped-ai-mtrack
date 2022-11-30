045d447fd8e1a30be14078f08aed5f64acaa894b
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-12-01 00:39:00 +0100

    quadratic

--- src/caped_ai_mtrack/RansacModels/quadratic_function.py
+++ src/caped_ai_mtrack/RansacModels/quadratic_function.py
@@ -1,4 +1,8 @@
+import math
+
 import numpy as np
+
+from ..Solvers import NewtonRaphson
 
 
 class QuadraticFunction:
@@ -50,3 +54,83 @@ class QuadraticFunction:
         self.coeff[2] = (
             delta[6] * theta[0] + delta[7] * theta[1] + delta[8] * theta[2]
         )
+
+    def distance(self, point):
+
+        x1 = point[1]
+        y1 = point[0]
+
+        a3 = 2 * self.coeff[0] * self.coeff[0]
+        a2 = 3 * self.coeff[1] * self.coeff[0] / a3
+        a1 = (
+            2 * self.coeff[2] * self.coeff[0]
+            - 2 * self.coeff[0] * y1
+            + 1
+            + self.coeff[1] * self.coeff[1]
+        ) / a3
+        a0 = (self.coeff[2] * self.coeff[1] - y1 * self.coeff[1] - x1) / a3
+
+        p = (3 * a1 - a2 * a2) / 3
+        q = (-9 * a1 * a2 + 27 * a0 + 2 * a2 * a2 * a2) / 27
+
+        tmp1 = np.sqrt(-p / 3)
+        tmp2 = q * q / 4 + p * p * p / 27
+
+        if tmp2 > 0:
+
+            aBar = np.cbrt(-q / 2 + np.sqrt(q * q / 4 + p * p * p / 27))
+            bBar = np.cbrt(-q / 2 - np.sqrt(q * q / 4 + p * p * p / 27))
+
+            xc1 = xc2 = xc3 = aBar + bBar - a2 / 3
+
+        elif tmp2 == 0:
+
+            if q > 0:
+
+                xc1 = -2 * tmp1
+                xc2 = tmp1
+                xc3 = xc2
+
+            elif q < 0:
+
+                xc1 = 2 * tmp1
+                xc2 = -tmp1
+                xc3 = xc2
+
+            else:
+
+                xc1 = 0
+                xc2 = 0
+                xc3 = 0
+
+        else:
+
+            if q >= 0:
+                phi = math.acos(-np.sqrt(q * q * 0.25 / (-p * p * p / 27)))
+            else:
+                phi = math.acos(math.sqrt(q * q * 0.25 / (-p * p * p / 27)))
+
+            xc1 = 2 * tmp1 * math.cos(phi / 3) - a2 / 3
+            xc2 = 2 * tmp1 * math.cos((phi + 2 * math.PI) / 3) - a2 / 3
+            xc3 = 2 * tmp1 * math.cos((phi + 4 * math.PI) / 3) - a2 / 3
+
+        returndistA = NewtonRaphson._distance(
+            x1,
+            y1,
+            xc1,
+            self.coeff[2] + self.coeff[1] * xc1 + self.coeff[0] * xc1 * xc1,
+        )
+        returndistB = NewtonRaphson._distance(
+            x1,
+            y1,
+            xc2,
+            self.coeff[2] + self.coeff[1] * xc2 + self.coeff[0] * xc2 * xc2,
+        )
+        returndistC = NewtonRaphson._distance(
+            x1,
+            y1,
+            xc3,
+            self.coeff[2] + self.coeff[1] * xc3 + self.coeff[0] * xc3 * xc3,
+        )
+
+        return math.min(returndistA, math.min(returndistB, returndistC))
