33127efc9d89e967f84081d319d66bea5d0f4dec
kapoorlab <randomaccessiblekapoor@gmail.com>
2022-12-25 16:47:14 +0100

    err

--- src/caped_ai_mtrack/Fits/comboransac.py
+++ src/caped_ai_mtrack/Fits/comboransac.py
@@ -116,22 +116,53 @@ class ComboRansac(Ransac):
                 (
                     inlier_points,
                     inliers_removed_from_starting,
-                    estimator_quad,
+                    estimator,
                 ) = ransac_first_quad
 
-                ransac_first_line = self.ransac_line.extract_first_ransac_line(
-                    inlier_points
-                )
-                if ransac_first_line is not None:
-                    (
-                        inlier_points_line,
-                        inliers_removed_from_starting_line,
-                        estimator,
-                    ) = ransac_first_line
             else:
                 starting_points = []
             estimators.append(estimator)
-            estimator_inliers.append(inlier_points_line)
+            estimator_inliers.append(inlier_points)
+            if len(starting_points) < self.min_samples:
+                print(
+                    "Not sufficeint inliers found %d , threshold=%d, therefore halting"
+                    % (len(starting_points), self.min_samples)
+                )
+
+                break
+            starting_points = inliers_removed_from_starting
+
+        # segments = clean_ransac(estimators, estimator_inliers)
+        # yarray, xarray = zip(*data_points_list)
+        # plot_ransac_gt(segments, yarray, xarray, save_name=self.save_name)
+
+        starting_points = np.asarray(estimator_inliers)
+
+        data_points_list = np.copy(estimator_inliers)
+        data_points_list = data_points_list.tolist()
+        estimators = []
+        estimator_inliers = []
+        for index in range(0, self.iterations):
+
+            if len(starting_points) <= self.min_samples:
+                print(
+                    "No more points available. Terminating search for RANSAC"
+                )
+                break
+            ransac_first_line = self.ransac_line.extract_first_ransac_line(
+                starting_points
+            )
+            if ransac_first_line is not None:
+                (
+                    inlier_points,
+                    inliers_removed_from_starting,
+                    estimator,
+                ) = ransac_first_line
+
+            else:
+                starting_points = []
+            estimators.append(estimator)
+            estimator_inliers.append(inlier_points)
             if len(starting_points) < self.min_samples:
                 print(
                     "Not sufficeint inliers found %d , threshold=%d, therefore halting"
